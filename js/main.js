fd362df8f3525fae22237f454ddd7d0f9bfe3364
PavelPrais <prais.pavel@gmail.com>
2022-03-02 05:29:27 +0300

    code refactoring in a file js

--- js/main.js
+++ js/main.js
@@ -3,48 +3,35 @@ var buttonMinus = document.getElementById('buttonMinus');
 var buttonMultiply = document.getElementById('buttonMultiply');
 var buttonDevide = document.getElementById('buttonDevide');
 
+var input1 = document.getElementById('number1');
+var input2 = document.getElementById('number2');
+
+function getNumber1() {
+  return Number(input1.value);
+}
+
+function getNumber2() {
+  return Number(input2.value);
+}
+
 function onButtonPlusClick() {
-  var input1 = document.getElementById('number1');
-  var input2 = document.getElementById('number2');
-
-  var number1 = Number(input1.value);
-  var number2 = Number(input2.value);
-
-  var summ = number1 + number2;
-  window.alert(summ);
+  var result = getNumber1() + getNumber2();
+  window.alert(result);
 };
 
 function onButtonMinusClick() {
-  var input1 = document.getElementById('number1');
-  var input2 = document.getElementById('number2');
-
-  var number1 = Number(input1.value);
-  var number2 = Number(input2.value);
-
-  var summ = number1 - number2;
-  window.alert(summ);
+  var result = getNumber1() - getNumber2();
+  window.alert(result);
 };
 
 function onButtonMultiplyClick() {
-  var input1 = document.getElementById('number1');
-  var input2 = document.getElementById('number2');
-
-  var number1 = Number(input1.value);
-  var number2 = Number(input2.value);
-
-  var summ = number1 * number2;
-  window.alert(summ);
+  var result = getNumber1() * getNumber2();
+  window.alert(result);
 };
 
 function onButtonDevideClick() {
-  var input1 = document.getElementById('number1');
-  var input2 = document.getElementById('number2');
-
-  var number1 = Number(input1.value);
-  var number2 = Number(input2.value);
-
-  var summ = number1 / number2;
-  window.alert(summ);
+  var result = getNumber1() / getNumber2();
+  window.alert(result);
 };
 
 buttonPlus.addEventListener('click', onButtonPlusClick);
