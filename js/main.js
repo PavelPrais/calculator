7ab5ad820b3a9793db67d7a758f5a75d637e603c
PavelPrais <prais.pavel@gmail.com>
2022-03-02 07:00:11 +0300

    third iteration of code refactoring in file js

--- js/main.js
+++ js/main.js
@@ -6,23 +6,18 @@ var buttonDevide = document.getElementById('buttonDevide');
 var input1 = document.getElementById('number1');
 var input2 = document.getElementById('number2');
 
-function getNumber1() {
-  return Number(input1.value);
-}
-
-function getNumber2() {
-  return Number(input2.value);
-}
-
 function makeOperation(operationCode) {
+  var number1 = Number(input1.value);
+  var number2 = Number(input2.value);
+
   if (operationCode === '+') {
-    var result = getNumber1() + getNumber2();
+    var result = number1 + number2;
   } else if (operationCode === '-') {
-    var result = getNumber1() - getNumber2();
+    var result = number1 - number2;
   } else if (operationCode === '*') {
-    var result = getNumber1() * getNumber2();
+    var result = number1 * number2;
   } else if (operationCode === '/') {
-    var result = getNumber1() / getNumber2();
+    var result = number1 / number2;
   } else {
     window.alert('operation is unknown')
   }
@@ -30,23 +25,13 @@ function makeOperation(operationCode) {
   window.alert(result);
 }
 
-function onButtonPlusClick() {
-  makeOperation('+');
-};
+function onOperationButtonClick(eventObject) {
+  var clickedElement = eventObject.currentTarget;
+  var operation = clickedElement.innerHTML;
+  makeOperation(operation);
+}
 
-function onButtonMinusClick() {
-  makeOperation('-');
-};
-
-function onButtonMultiplyClick() {
-  makeOperation('*');
-};
-
-function onButtonDevideClick() {
-  makeOperation('/');
-};
-
-buttonPlus.addEventListener('click', onButtonPlusClick);
-buttonMinus.addEventListener('click', onButtonMinusClick);
-buttonMultiply.addEventListener('click', onButtonMultiplyClick);
-buttonDevide.addEventListener('click', onButtonDevideClick);
+buttonPlus.addEventListener('click', onOperationButtonClick);
+buttonMinus.addEventListener('click', onOperationButtonClick);
+buttonMultiply.addEventListener('click', onOperationButtonClick);
+buttonDevide.addEventListener('click', onOperationButtonClick);
