ac127a17a1a31fda3e1c0144187e3f4c142e53cc
PavelPrais <prais.pavel@gmail.com>
2022-03-03 06:34:03 +0300

    fifth iteration of code refactoring in js file

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
-var buttonPlus = document.getElementById('buttonPlus');
-var buttonMinus = document.getElementById('buttonMinus');
-var buttonMultiply = document.getElementById('buttonMultiply');
-var buttonDevide = document.getElementById('buttonDevide');
+var operationButtons = document.getElementsByClassName('operation-button');
 
 var input1 = document.getElementById('number1');
 var input2 = document.getElementById('number2');
@@ -30,8 +27,6 @@ function onOperationButtonClick(eventObject) {
   var operation = clickedElement.innerHTML;
   makeOperation(operation);
 }
-
-var operationButtons = [buttonPlus, buttonMinus, buttonMultiply, buttonDevide];
 
 for (var i = 0; i < operationButtons.length; i++) {
   var button = operationButtons[i];
